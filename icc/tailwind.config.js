60e255c609bcb601bbd8c567ec0b947b7239ea5e
didadejan <108562664+didadejan@users.noreply.github.com>
2022-07-16 20:28:34 +0700

    home
    home

---
+++ icc/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3F4E4F',
+        secondary: '#A27B5C',
+        dark: '#2C3639',
+        four: '#DCD7C9',
+        navbar: 'rgba(0, 0, 0, .9)'
+      },
+    },
+  },
+  plugins: [],
+}
